f4e2c3332660bef4046400c60ac73ab876a6c3a3
yamnyk <notAunicorN2018>
2018-09-11 03:26:08 +0300

    add all blocks without last block carousel, add not finished modal window

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+////////////////TABS//////////////
+$(".services-tab-item").not(":first").hide();
+$(".services-tab-link").click(function() {
+    $(".services-tab-link").removeClass("services-tab-active").eq($(this).index()).addClass("services-tab-active");
+    $(".services-tab-item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("services-tab-active");
+
+$('ham-button').click(function (event) {
+    $('')
+});
+///////////MODAL/////////////////
+$("#btn-mdl").click(function(e){
+    $('.modal').addClass('modal-win');
+    $(".modal-overlay").addClass('overlay-open');
+});
+$(".modal-overlay").click(function(e){
+    // console.log($(e.target).hasClass('overlay'));
+    $('.modal').removeClass('modal-win');
+    $(".modal-overlay").removeClass('overlay-open');
+});
